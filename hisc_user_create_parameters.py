d1cddea886557e41c193706c94072e0f3036ffc2
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2018-06-14 14:10:26 -0500

    Don't worry about franchises for now

--- hisc_user_create_parameters.py
+++ hisc_user_create_parameters.py
@@ -1,7 +1,7 @@
 from azure.graphrbac.models import UserBase
 
-class HISCUserCreateParameters(UserBase):
 
+class HISCUserCreateParameters(UserBase):
     _validation = {
         'account_enabled': {'required': True},
         'display_name': {'required': True},
@@ -26,35 +26,35 @@ class HISCUserCreateParameters(UserBase):
         'city': {'key': 'city', 'type': 'str'},
         'state': {'key': 'state', 'type': 'str'},
         'country': {'key': 'country', 'type': 'str'},
-        'mail': {'key': 'mail', 'type': 'str'}, # Only useable if we actually have the mail attribute
-        'creation_type': {'key': 'creationType', 'type': 'str'}, # Must be "LocalAccount"
+        'mail': {'key': 'mail', 'type': 'str'},  # Only useable if we actually have the mail attribute
+        'creation_type': {'key': 'creationType', 'type': 'str'},  # Must be "LocalAccount"
         'sign_in_names': {'key': 'signInNames', 'type': '[SignInName]'},
     }
 
-    def __init__(self, 
-        account_enabled, 
-        display_name, 
-        password_profile, 
-        user_principal_name, 
-        mail_nickname, 
-        additional_properties=None,
-        immutable_id=None, 
-        usage_location=None, 
-        given_name=None, 
-        surname=None, 
-        user_type=None, 
-        mail=None,
-        other_mails=None,
-        city=None,
-        state=None,
-        country=None,
-        creation_type=None,
-        sign_in_names=None):
+    def __init__(self,
+                 account_enabled,
+                 display_name,
+                 password_profile,
+                 user_principal_name,
+                 mail_nickname,
+                 additional_properties=None,
+                 immutable_id=None,
+                 usage_location=None,
+                 given_name=None,
+                 surname=None,
+                 user_type=None,
+                 mail=None,
+                 other_mails=None,
+                 city=None,
+                 state=None,
+                 country=None,
+                 creation_type=None,
+                 sign_in_names=None):
         super(HISCUserCreateParameters, self).__init__(
-            immutable_id=immutable_id, 
-            usage_location=usage_location, 
-            given_name=given_name, 
-            surname=surname, 
+            immutable_id=immutable_id,
+            usage_location=usage_location,
+            given_name=given_name,
+            surname=surname,
             user_type=user_type,
             additional_properties=additional_properties)
         self.account_enabled = account_enabled
--- user_operations.py
+++ user_operations.py
@@ -3,49 +3,61 @@ from azure.graphrbac import GraphRbacManagementClient
 from azure.graphrbac.models import PasswordProfile, SignInName, UserUpdateParameters
 from hisc_user_create_parameters import HISCUserCreateParameters
 
-def create_user(fname, lname, email, franchises, config):
-	return client(config).users.create(_user_create_parameters(fname, lname, email, franchises, config))
+
+def create_user(fname, lname, email, config):
+    return client(config).users.create(_user_create_parameters(fname, lname, email, config))
+
 
 def update_user(object_id, franchises, config):
-	return client(config).users.update(object_id, _user_update_parameters(franchises))
+    return client(config).users.update(object_id, _user_update_parameters(franchises))
+
 
 def client(config):
-	return GraphRbacManagementClient(_credentials(config), config['tenant_id'])
+    return GraphRbacManagementClient(_credentials(config), config['tenant_id'])
+
 
 def _user_update_parameters(franchises):
-	return UserUpdateParameters(additional_properties={'extension_e26ba073fd5142af9fa6455daecdcc9c_franchises': franchises})
+    return UserUpdateParameters(
+        additional_properties={'extension_e26ba073fd5142af9fa6455daecdcc9c_franchises': franchises})
 
-def _user_create_parameters(fname, lname, email, franchises, config):
-	return HISCUserCreateParameters(
-		additional_properties={'extension_e26ba073fd5142af9fa6455daecdcc9c_franchises': franchises},
-		account_enabled=True,
-		display_name='{}.{}'.format(fname, lname),
-		password_profile=_password_profile(config),
-		user_principal_name=_principle_name(email, config),
-		mail_nickname=_mail_nickname(email),
-		given_name=fname,
-		surname=lname,
-		user_type='Guest',
-		other_mails=[email],
-		sign_in_names=[_sign_in_name(email)],
-		usage_location='US',
-		creation_type='LocalAccount'
+
+def _user_create_parameters(fname, lname, email, config):
+    return HISCUserCreateParameters(
+        account_enabled=True,
+        display_name='{}.{}'.format(fname, lname),
+        password_profile=_password_profile(config),
+        user_principal_name=_principle_name(email, config),
+        mail_nickname=_mail_nickname(email),
+        given_name=fname,
+        surname=lname,
+        user_type='Guest',
+        other_mails=[email],
+        sign_in_names=[_sign_in_name(email)],
+        usage_location='US',
+        creation_type='LocalAccount'
     )
 
+
 def _principle_name(email, config):
-	return config['principle_template'].format(_mail_nickname(email))
+    return config['principle_template'].format(_mail_nickname(email))
+
 
 def _mail_nickname(email):
-	return '{}#EXT#'.format(_format_email(email))
+    return '{}#EXT#'.format(_format_email(email))
+
 
 def _format_email(email):
-	return email.replace('@', '_')
+    return email.replace('@', '_')
+
 
 def _credentials(config):
-	return ServicePrincipalCredentials(client_id=config['client_id'], secret=config['client_key'], tenant=config['tenant_id'], resource=config['ad_graph_url'])
+    return ServicePrincipalCredentials(client_id=config['client_id'], secret=config['client_key'],
+                                       tenant=config['tenant_id'], resource=config['ad_graph_url'])
+
 
 def _password_profile(config):
-	return PasswordProfile(password=config['default_password'], force_change_password_next_login=True)
+    return PasswordProfile(password=config['default_password'], force_change_password_next_login=True)
+
 
 def _sign_in_name(email):
-	return SignInName(type='emailAddress', value=email)
+    return SignInName(type='emailAddress', value=email)
