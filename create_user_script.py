67a3a16a052c8c6d6207c67ece6211fb6c6ea5a0
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2018-06-14 14:10:39 -0500

    Create standalone script to create user

--- create_user_script.py
+++ create_user_script.py
@@ -1,8 +1,19 @@
 import click
+import yaml
+
+from user_operations import create_user
 
 
-def create():
-    pass
+@click.command()
+@click.argument('first_name')
+@click.argument('last_name')
+@click.argument('email')
+def create(first_name, last_name, email):
+    with open("config/config.yml", 'r') as ymlfile:
+        config = yaml.load(ymlfile)
+
+    result = create_user(first_name, last_name, email, config)
+    print(result)
 
 
 if __name__ == "__main__":
